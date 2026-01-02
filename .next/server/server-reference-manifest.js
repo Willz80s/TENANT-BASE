bb8b7da976201b12d4142bb8ecda02f293aa9611
Willz80s <ojiegodswill@gmail.com>
2026-01-02 08:48:35 -0800

    Project scaffold

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jSGDSkCkwBeKx3amvzGRBl6QvBANzzds797CuzEsw6g=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Tenant Base</h1>
+      <p>SaaS Admin Platform</p>
+    </main>
+  );
+}
